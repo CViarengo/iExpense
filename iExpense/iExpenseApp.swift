c75ac152622786b88e4e91126090ecfc3bdcda74
CViarengo <100734875+CViarengo@users.noreply.github.com>
2022-03-26 13:30:28 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Chris Viarengo on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
